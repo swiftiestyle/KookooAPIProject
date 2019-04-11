cf43992e542d02f073c06f9e6c48e5ac613c7146
Udit Goel <uditgoel.82@gmail.com>
2019-04-11 21:44:49 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");    
+const favicon = require('serve-favicon');
 const app= express();
 const port= 5000;
 var xmlBody = require('./kookoo.js');
-
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get('/', function(req, res){
   res.header('Content-Type', 'text/xml');
   res.send(xmlBody.getXMLBody(req));
